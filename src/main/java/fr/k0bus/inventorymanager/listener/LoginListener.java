9a6ef0c775e823f8f641aed09c8e7379873afeb9
K0bus <contact@k0bus.fr>
2025-03-05 00:32:47 +0100

    Remove unused import

--- src/main/java/fr/k0bus/inventorymanager/listener/LoginListener.java
+++ src/main/java/fr/k0bus/inventorymanager/listener/LoginListener.java
@@ -1,14 +1,8 @@
 package fr.k0bus.inventorymanager.listener;
 
 import fr.k0bus.inventorymanager.InventoryManager;
-import fr.xephi.authme.AuthMe;
-import fr.xephi.authme.api.v3.AuthMeApi;
-import fr.xephi.authme.settings.properties.AuthMeSettingsRetriever;
-import fr.xephi.authme.settings.properties.RestrictionSettings;
-import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerChangedWorldEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 
 public class LoginListener implements Listener {
